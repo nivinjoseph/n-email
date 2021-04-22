885634149d5e97330860cdf84231c5d5d77e099a
nivinjoseph <nivinjoseph@outlook.com>
2021-04-21 23:34:05 -0400

    refactorings

--- dist/scripts/send-grid-email-service.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class SendGridEmailService {
-    private readonly _senderEmail;
-    constructor();
-    sendEmail(toEmail: string, subject: string, body: string, isBodyHtml?: boolean): Promise<void>;
-}

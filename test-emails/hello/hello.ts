2a22941031faaf337829e7334a291af31b344cb3
nivinjoseph <nivinjoseph@outlook.com>
2021-05-02 22:36:42 -0400

    package updates

--- test-emails/hello/hello.ts
+++ test-emails/hello/hello.ts
@@ -4,5 +4,5 @@ import "./hello.mjml";
 export default {
     // "$primary-color": "#F63A4D",
     "image:$hero-background-image": "./images/default-twitter-card-image.jpg",
-    "image:$squirrel-image": "./images/wrise-squirrel-colored.png?format=jpeg&background=#F45E43"
+    "image:$squirrel-image": "./images/wrise-squirrel-colored.png?format=jpeg&background=F45E43"
 };
